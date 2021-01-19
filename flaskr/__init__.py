ef15c31c11182cd940524162f0d2446c3c556303
korosh <korosh.khosravi69@gmail.com>
2021-01-19 23:51:58 +0330

    new article

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,33 +10,135 @@ def home():
 # ####################### butterfly ####################### #
 
 @app.route('/butterfly/arm_drills')
-def arm():
+def butterfly_arm():
     return render_template('blog/butterfly/arm.html')
 
 @app.route('/butterfly/kick_drills')
-def kick():
+def butterfly_kick():
     return render_template('blog/butterfly/kick.html')
 
 @app.route('/butterfly/body_position_drills')
-def body_position():
+def butterfly_body_position():
     return render_template('blog/butterfly/body-position.html')
 
 @app.route('/butterfly/breathing_drills')
-def breathing():
+def butterfly_breathing():
     return render_template('blog/butterfly/breathing.html')
 
 @app.route('/butterfly/coordination_drills')
-def coordination():
+def butterfly_coordination():
     return render_template('blog/butterfly/coordination.html')
 
 @app.route('/butterfly/leverage_drills')
-def leverage():
+def butterfly_leverage():
     return render_template('blog/butterfly/leverage.html')
 
 @app.route('/butterfly/recovery_drills')
-def recovery():
+def butterfly_recovery():
     return render_template('blog/butterfly/recovery.html')
 
+# ####################### breaststroke ####################### #
+
+
+@app.route('/breaststroke/arm_drills')
+def breaststroke_arm():
+    return render_template('blog/breaststroke/arm.html')
+
+@app.route('/breaststroke/kick_drills')
+def breaststroke_kick():
+    return render_template('blog/breaststroke/kick.html')
+
+@app.route('/breaststroke/body_position_drills')
+def breaststroke_body_position():
+    return render_template('blog/breaststroke/body-position.html')
+
+@app.route('/breaststroke/breathing_drills')
+def breaststroke_breathing():
+    return render_template('blog/breaststroke/breathing.html')
+
+@app.route('/breaststroke/coordination_drills')
+def breaststroke_coordination():
+    return render_template('blog/breaststroke/coordination.html')
+
+@app.route('/breaststroke/leverage_drills')
+def breaststroke_leverage():
+    return render_template('blog/breaststroke/leverage.html')
+
+@app.route('/breaststroke/recovery_drills')
+def breaststroke_recovery():
+    return render_template('blog/breaststroke/recovery.html')
+
+# ####################### backstroke ####################### #
+
+
+@app.route('/backstroke/arm_drills')
+def backstroke_arm():
+    return render_template('blog/backstroke/arm.html')
+
+@app.route('/backstroke/kick_drills')
+def backstroke_kick():
+    return render_template('blog/backstroke/kick.html')
+
+@app.route('/backstroke/body_position_drills')
+def backstroke_body_position():
+    return render_template('blog/backstroke/body-position.html')
+
+@app.route('/backstroke/breathing_drills')
+def backstroke_breathing():
+    return render_template('blog/backstroke/breathing.html')
+
+@app.route('/backstroke/coordination_drills')
+def backstroke_coordination():
+    return render_template('blog/backstroke/coordination.html')
+
+@app.route('/backstroke/leverage_drills')
+def backstroke_leverage():
+    return render_template('blog/backstroke/leverage.html')
+
+@app.route('/backstroke/recovery_drills')
+def backstroke_recovery():
+    return render_template('blog/backstroke/recovery.html')
+
+# ####################### freestyle ####################### #
+
+
+@app.route('/freestyle/arm_drills')
+def freestyle_arm():
+    return render_template('blog/freestyle/arm.html')
+
+@app.route('/freestyle/kick_drills')
+def freestyle_kick():
+    return render_template('blog/freestyle/kick.html')
+
+@app.route('/freestyle/body_position_drills')
+def freestyle_body_position():
+    return render_template('blog/freestyle/body-position.html')
+
+@app.route('/freestyle/breathing_drills')
+def freestyle_breathing():
+    return render_template('blog/freestyle/breathing.html')
+
+@app.route('/freestyle/coordination_drills')
+def freestyle_coordination():
+    return render_template('blog/freestyle/coordination.html')
+
+@app.route('/freestyle/leverage_drills')
+def freestyle_leverage():
+    return render_template('blog/freestyle/leverage.html')
+
+@app.route('/freestyle/recovery_drills')
+def freestyle_recovery():
+    return render_template('blog/freestyle/recovery.html')
+
+# #######################  ####################### #
+
+@app.route('/Wim_Hof_Method')
+def article():
+    return render_template('blog/article/Wim Hof Method.html')
+
+@app.route('/Winter_mixed')
+def Winter_mixed():
+    return render_template('blog/article/Winter_mixed.html')
 
 
 
@@ -44,11 +146,18 @@ def recovery():
 
 
 
+# #######################  ####################### #
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
 
+@app.route('/forget_password')
+def forget():
+    return render_template('auth/forget.html')
 
-
-
-
+@app.route('/register')
+def register():
+    return render_template('auth/register.html')
 
 
 
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-from flask import Flask, redirect, url_for, render_template, Blueprint
-
-app = Flask(__name__, template_folder='flaskr/templates')
-
-c = ['korosh', 'maryam', 'khosrow',"armin"]
-
-@app.route('/')
-def home():
-    return render_template('index.html', content=c)
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+    def sub(self, r):
+        return sum(self.r * 2)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+x = Complex
+x.sub(2)
+print(x.sub)
