8581186dc0740f17ac32236816f34d8b42578016
korosh <korosh.khosravi69@gmail.com>
2021-01-26 13:29:20 +0330

    host

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.static_folder = 'static'
 app.template_folder = 'templates'
 # app.config['SERVER_NAME, SERVER_NAME'] = 'www.swimmingworld.ir, swimmingworld.ir'
-app.config['SERVER_NAME, SERVER_NAME'] = '127.0.0.1:5000, localhost:5000'
+# app.config['SERVER_NAME, SERVER_NAME'] = '127.0.0.1:5000, localhost:5000'
 
 
 
